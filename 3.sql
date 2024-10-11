aae1c42088af7fe622526a49461a431cb6d5dc10
Xenia Gontar <118064189+xenia-gontar@users.noreply.github.com>
2024-10-11 13:44:29 +0300

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,9 +1,8 @@
 SELECT e.name,
   c.tax_percentage * p.salary as tax
 FROM  employees as e
-FULL OUTER JOIN contracts as c ON c.id = e.id
-FULL OUTER JOIN positions as p ON p.id = e.id
-GROUP BY e.name
+FULL OUTER JOIN contracts as c ON c.id = e.contract_id
+FULL OUTER JOIN positions as p ON p.id = e.position_id
 WHERE p.salary < 50000
 
 
